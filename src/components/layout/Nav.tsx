ceb9ce8d58176a2f76d2aa849fe29ab893fa42dc
HyunHo Lee <dlgusgh2001@naver.com>
2022-02-07 18:20:10 +0900

    🎉 초기 셋팅(craco, typescript, styled-components)

---
+++ src/components/layout/Nav.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Nav = () => {
+  return <div>네이게이션 바 컴포넌트</div>;
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const COLOR = {
+  BLUE: '#1565C0',
+  BLUE_LIGHT: '#2196F3',
+  TAG: '#FFA000',
+  GRAY: '#939FA5',
+};
