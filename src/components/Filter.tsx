1d819027aafd867e3845b1dfb3463d1d615eed9a
HyunHo Lee <dlgusgh2001@naver.com>
2022-02-08 17:57:08 +0900

    ✨Select 박스 인풋 라벨 데이터 연결

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,8 +1,7 @@
 import { COLOR } from 'constants/';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Estimate } from 'types/card';
-import { Set } from 'typescript';
 
 interface Props {
   apiData: Estimate[];
@@ -13,23 +12,54 @@ export const Filter = ({ apiData }: Props) => {
   const materialArr = apiData.map(data => data.material).flat(Infinity);
   const methodSet = Array.from(new Set(methodArr));
   const materialSet = Array.from(new Set(materialArr));
+  const [method, setMethod] = useState(false);
+  const [material, setMaterial] = useState(false);
+  const [selectMethod, setSelectMethod] = useState([]);
+  const [selectMaterial, setSelectMaterial] = useState([]);
+
+  const onCheckMethod = (e: any, data: string | []) => {
+    // const newSelect = selectMethod.push(data);
+    console.log(data);
+  };
+
+  const onMethod = () => {
+    setMethod(!method);
+  };
+  const onMaterial = () => {
+    setMaterial(!material);
+  };
+
   return (
     <FilterBox>
       <FilterUl>
-        <li>가공방식</li>
-        <ListUl>
-          {methodSet.map(method => (
-            <li>{method}</li>
-          ))}
-        </ListUl>
+        <Select onClick={onMethod}>가공방식</Select>
+        {method && (
+          <ListUl>
+            {methodSet.map((data, i) => (
+              <List key={`method-${i}`}>
+                <input
+                  onChange={e => onCheckMethod(e, data)}
+                  type="checkbox"
+                  id={`${data}`}
+                />
+                <label htmlFor={`${data}`}>{data}</label>
+              </List>
+            ))}
+          </ListUl>
+        )}
       </FilterUl>
       <FilterUl>
-        <li>재료</li>
-        <ListUl>
-          {materialSet.map(material => (
-            <li>{material}</li>
-          ))}
-        </ListUl>
+        <Select onClick={onMaterial}>재료</Select>
+        {material && (
+          <ListUl>
+            {materialSet.map((material, i) => (
+              <List key={`material-${i}`}>
+                <input type="checkbox" id={`${material}`} />
+                <label htmlFor={`${material}`}>{material}</label>
+              </List>
+            ))}
+          </ListUl>
+        )}
       </FilterUl>
     </FilterBox>
   );
@@ -40,12 +70,12 @@ const FilterBox = styled.ul`
 `;
 
 const FilterUl = styled.ul`
+  width: 98px;
+  height: 32px;
   position: relative;
   border: 1px solid ${COLOR.GRAY};
   border-radius: 4px;
   box-sizing: border-box;
-  /* width: 98px; */
-  height: 32px;
   line-height: 23px;
   padding: 4px 12px;
   cursor: pointer;
@@ -53,8 +83,10 @@ const FilterUl = styled.ul`
     margin-right: 1rem;
   }
   & > li {
+    display: flex;
+    align-items: center;
     font-size: 12px;
-    margin-right: 2rem;
+    padding-left: 15px;
   }
   & > li::after {
     content: '';
@@ -72,10 +104,31 @@ const FilterUl = styled.ul`
     border: 1px solid ${COLOR.BLUE_LIGHT};
   }
 `;
-
+const Select = styled.li`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
 const ListUl = styled.ul`
   position: absolute;
-  display: none;
-  border: 1px solid dodgerblue;
+  top: 32px;
+  left: 0;
+  padding: 8px 5px;
+  border: 1px solid ${COLOR.GRAY};
   width: 100%;
+  border-radius: 4px;
+  background-color: #fff;
+  z-index: 100;
+`;
+const List = styled.li`
+  display: flex;
+  margin: 5px 0;
+  label {
+    flex: 1;
+    cursor: pointer;
+    font-size: 14px;
+    line-height: 1.2;
+  }
 `;
